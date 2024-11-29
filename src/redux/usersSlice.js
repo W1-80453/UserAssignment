7c6bbf6f09322718459011ac1262242270b202bd
W1-80453 <pratikshakolawale3@gmail.com>
2024-11-29 20:00:57 +0530

    Configure Redux Toolkit

---
+++ src/redux/usersSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  return response.json();
+});
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: { users: [], loading: false },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+      });
+  },
+});
+
+export const selectUsers = (state) => state.users.users;
+export const selectLoading = (state) => state.users.loading;
+export default usersSlice.reducer;
